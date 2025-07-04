3130dc13c78c82efebec7d799416909293d541eb
Jacobliterator <jacobliterator@outlook.com>
2025-07-04 18:39:55 -0400

    new blocks and items

--- src/main/java/net/jacobliterator/tutorialmod/TutorialMod.java
+++ src/main/java/net/jacobliterator/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package net.jacobliterator.tutorialmod;
 
+import net.jacobliterator.tutorialmod.block.ModBlocks;
+import net.jacobliterator.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -37,6 +40,9 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -59,7 +65,14 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
