0f2559193a91d6035425519d7b4d4af20410f6e3
Jacobliterator <jacobliterator@outlook.com>
2025-07-06 14:43:24 -0400

    new chisel block :)

--- src/main/java/net/jacobliterator/tutorialmod/item/ModItems.java
+++ src/main/java/net/jacobliterator/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jacobliterator.tutorialmod.item;
 
 import net.jacobliterator.tutorialmod.TutorialMod;
+import net.jacobliterator.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -14,6 +15,8 @@ public class ModItems {
 
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
