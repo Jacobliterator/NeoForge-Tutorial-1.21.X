e5c4d2e63d3d9a35d2d98f3bf381b0652f1e9964
Jacobliterator <jacobliterator@outlook.com>
2025-07-29 23:03:07 -0400

    shit i forgot

--- src/main/java/net/jacobliterator/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/jacobliterator/tutorialmod/datagen/ModItemModelProvider.java
@@ -1,10 +1,16 @@
 package net.jacobliterator.tutorialmod.datagen;
 
 import net.jacobliterator.tutorialmod.TutorialMod;
+import net.jacobliterator.tutorialmod.block.ModBlocks;
 import net.jacobliterator.tutorialmod.item.ModItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.WallBlock;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
 
 public class ModItemModelProvider  extends ItemModelProvider {
     public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -21,5 +27,28 @@ public class ModItemModelProvider  extends ItemModelProvider {
         basicItem(ModItems.FROSTFIRE_ICE.get());
         basicItem(ModItems.CHISEL.get());
 
+        buttonItem(ModBlocks.BISMUTH_BUTTON, ModBlocks.BISMUTH_BLOCK);
+        fenceItem(ModBlocks.BISMUTH_FENCE, ModBlocks.BISMUTH_BLOCK);
+        wallItem(ModBlocks.BISMUTH_WALL, ModBlocks.BISMUTH_BLOCK);
+
+        basicItem(ModBlocks.BISMUTH_DOOR.asItem());
+
+    }
+    public void buttonItem(DeferredBlock<?> block, DeferredBlock<Block> baseBlock) {
+        this.withExistingParent(block.getId().getPath(), mcLoc("block/button_inventory"))
+                .texture("texture",  ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
+                        "block/" + baseBlock.getId().getPath()));
+    }
+
+    public void fenceItem(DeferredBlock<?> block, DeferredBlock<Block> baseBlock) {
+        this.withExistingParent(block.getId().getPath(), mcLoc("block/fence_inventory"))
+                .texture("texture",  ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
+                        "block/" + baseBlock.getId().getPath()));
+    }
+
+    public void wallItem(DeferredBlock<?> block, DeferredBlock<Block> baseBlock) {
+        this.withExistingParent(block.getId().getPath(), mcLoc("block/wall_inventory"))
+                .texture("wall",  ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID,
+                        "block/" + baseBlock.getId().getPath()));
     }
 }
